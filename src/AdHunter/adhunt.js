bbdabd7e1ad7a9d685cd4b7b74f1de70eef33408
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2020-11-11 19:39:04 +0000

    Created REST API for extension and server     * Extension sends a post request to the server with the details of a       network request made to an ad tracking server     * Server receives data and prints it out on the console

--- src/AdHunter/adhunt.js
+++ src/AdHunter/adhunt.js
@@ -1,8 +1,16 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        console.log("Block: ", details.url)
+        console.log("Block: ", details)
+        fetch('http://localhost:5000/extension', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(details),
+            })
+            .then(response => console.log(response))
         return {cancel: true}
     },
     {urls: blocked_domains},
-    ["blocking"]
+    ["requestBody"]
 )
--- src/server/index.js
+++ src/server/index.js
@@ -5,10 +5,16 @@ const cors = require('cors');
 app = express();
 
 app.use(cors());
+app.use(express.json())
 
 app.get("/index", async(req, res)=>{
     res.send("Server says hi!");
 })
+
+app.post('/extension', function (req, res) {
+    res.send('POST request to the extensions');
+    console.log(req.body);
+  })
 
 app.listen(5000, ()=>{
     console.log("Server has started");
