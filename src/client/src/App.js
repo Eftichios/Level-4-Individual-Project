e7cd6beab32ce79f7e734e663619037edc92773d
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2020-12-07 12:41:35 +0000

    Fixed redirection bug when navigating through the app, closes #35     * Components were being rendered before authorisation check finished     * Changed app component to be a react class     * Added loader when page is waiting for authorisation checks

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,69 +1,79 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './App.css';
 import {Register} from './Components/Authentication/Register';
 import {Login} from './Components/Authentication/Login';
 import {BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import Navigation from "./Components/Navigation";
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Lobby from "./Components/Lobby/Lobby";
 import Summary from "./Components/Summary/Summary";
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from 'react-loader-spinner'
 
 toast.configure();
 
-function App() {
+export class App extends React.Component {
   
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      isAuthenticated: true,
+      isLoading: true
+    }
+    
+    this.setAuth = (token_exists) => {
+      this.setState({isAuthenticated: token_exists});
+    };
+  }
 
-  const setAuth = (token_exists) => {
-    setIsAuthenticated(token_exists);
-  };
+  componentDidMount() {
+    this.isAuth();
+  }
 
-  async function isAuth(){
+  isAuth = async () => {
     try{
-
       const response = await fetch("http://localhost:5000/auth/is-verify", {
         method: "GET",
         headers: {token: localStorage.token}
       });
 
       const parseRes = await response.json();
-
-      parseRes === true? setAuth(true): setAuth(false);
-
+      parseRes===true? this.setState({isAuthenticated:true}) : this.setState({isAuthenticated:false});
+      this.setState({isLoading: false});
+      
     } catch (err){
+      this.setState({isLoading: false});
       console.error(err.message);
     }
   }
 
-  useEffect(()=>{
-    isAuth();
-  })
+  render(){
+    if (this.state.isLoading) {
 
-  return (
-    <div className="d-flex justify-content-center">
-      <div className="mt-4 card card-main">
-      <div className="card-body">
-      <input type="hidden" autofocus={true} /> {/* Put this here to remove autofocus */}
-      <Router>
-        <Switch>
-          <Route path="/register" render={props => !isAuthenticated? <Register {...props} setAuth={setAuth} />:
-            <Redirect to="/login" />} />
-          <Route path="/login" render={props => !isAuthenticated? <Login {...props} setAuth={setAuth} /> : 
-            <Redirect to="/dashboard" />} />
-          <Route path="/dashboard" render={props => isAuthenticated? <Dashboard {...props} setAuth={setAuth} />:
-            <Redirect to="/login" />} />
-          <Route path="/lobby" render={props => isAuthenticated? <Lobby {...props} setAuth={setAuth} />:
-            <Redirect to="/login"/>} />
-          <Route path="/summary" render={props => isAuthenticated? <Summary {...props} setAuth={setAuth} />:
-            <Redirect to="/login" />} />
-        </Switch>
-      </Router>
+      return <Loader className="loader-middle" type="Oval" color="#F89603" height={80} width={80}/>
+    }
+
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="mt-4 card card-main">
+        <div className="card-body">
+        <input type="hidden" autofocus={true} /> {/* Put this here to remove autofocus */}
+        <Router>
+          <Switch>
+            <Route path="/register" render={props => !this.state.isAuthenticated? <Register {...props} setAuth={this.setAuth} />:<Redirect to="/login" />} />
+            <Route path="/login" render={props => !this.state.isAuthenticated? <Login {...props} setAuth={this.setAuth} /> : <Redirect to="/dashboard" />} />
+            <Route path="/dashboard" render={props => this.state.isAuthenticated? <Dashboard {...props} setAuth={this.setAuth} />:<Redirect to="/login" />} />
+            <Route path="/lobby" render={props => this.state.isAuthenticated? <Lobby {...props} setAuth={this.setAuth} />:<Redirect to="/login"/>} />
+            <Route path="/summary" render={props => this.state.isAuthenticated? <Summary {...props} setAuth={this.setAuth} />:<Redirect to="/login" />} />
+            <Route path="/test" render={props => this.state.isAuthenticated? <h3>AUTHENTICATED</h3>:<h3>NOT AUTHENTICATED</h3>} />
+          </Switch>
+        </Router>
+        </div>
+        </div>
       </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
