3d32268716b60b945b06f4a4a2ffaa8ee88f99e9
Eftichios <eftichios1997@hotmail.com>
2021-01-25 16:02:23 +0000

    Attempting to fix socket reconnecting

--- client/src/Components/Utilities/socketConfig.js
+++ client/src/Components/Utilities/socketConfig.js
@@ -1,7 +1,9 @@
 import io from 'socket.io-client';
 
 // we define the socket here so that we can share it between components
-var socket = io.connect('https://ad-hunter.herokuapp.com/');
+if (socket===undefined){
+    var socket = io.connect('https://ad-hunter.herokuapp.com/');
+}
 
 export default socket;
 
