b7acb910d1a95713d41e0bcd3bc6e5d81a9f4dbf
Eftichios <eftichios1997@hotmail.com>
2021-01-25 20:03:39 +0000

    Fix socket connecting to wrong url

--- client/src/Components/Utilities/socketConfig.js
+++ client/src/Components/Utilities/socketConfig.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 // we define the socket here so that we can share it between components
 if (socket===undefined){
-    var socket = io.connect("http://localhost:5000", {transports: ['websocket'], upgrade: false});
+    var socket = io({transports: ['websocket'], upgrade: false});
 }
 
 console.log(socket);
