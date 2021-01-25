247a8f2ff2f76f56d183623678659ea2bcf3dc04
Eftichios <eftichios1997@hotmail.com>
2021-01-25 19:57:44 +0000

    Set socket upgrade to false

--- client/src/Components/Utilities/socketConfig.js
+++ client/src/Components/Utilities/socketConfig.js
@@ -2,10 +2,10 @@ import io from 'socket.io-client';
 
 // we define the socket here so that we can share it between components
 if (socket===undefined){
-    var socket = io({transports: ['websocket'], reconnection: false});
+    var socket = io.connect("http://localhost:5000", {transports: ['websocket'], upgrade: false});
 }
 
-console.log(socket.id);
+console.log(socket);
 
 
 export default socket;
