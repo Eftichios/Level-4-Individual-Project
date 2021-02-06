bdb9a5dee30afdb4eead096e7be2d8e2064bb4a5
Eftichios <eftichios1997@hotmail.com>
2021-02-06 19:23:27 +0000

    Deploy: Fix leaderboards search API url

--- client/src/Components/Dashboard/DashLead.js
+++ client/src/Components/Dashboard/DashLead.js
@@ -23,7 +23,7 @@ export class DashLead extends React.Component {
         e.preventDefault();
         
         try {
-          const response = await fetch(`http://localhost:5000/api/userMetrics/search`, {
+          const response = await fetch(`/api/userMetrics/search`, {
             method:"POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({user: this.state.search_query})
