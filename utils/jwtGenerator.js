36e26bf6ec06055076e6d193672b288f51e351df
Eftichios <eftichios1997@hotmail.com>
2021-02-23 18:00:59 +0000

    Deploy: Increase jwt token expiration time

--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -7,7 +7,7 @@ function jwtGenerator(user_id){
         user: user_id
     }
 
-    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "2hr"});
+    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "8hr"});
 }
 
 module.exports = jwtGenerator;
