ff9c747b6b63ca0462b6dae855c2be3587ac2e25
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2021-04-06 21:57:38 +0100

    Removed test that relies on external service

--- src/server/test/category.test.js
+++ src/server/test/category.test.js
@@ -7,17 +7,17 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("Category mode API", ()=>{
-    it("It should return Sports", async ()=>{
-        const url = "www.joindota.com"
-        const response = await chai.request(app)
-                                   .post("/api/category")
-                                   .set('content-type', 'application/json')
-                                   .send({ad_url: url});
-        response.should.have.status(200)
-        response.body.length.should.equal(1)
-        response.body[0].should.equal("Sports")
+    // it("It should return Sports", async ()=>{
+    //     const url = "www.joindota.com"
+    //     const response = await chai.request(app)
+    //                                .post("/api/category")
+    //                                .set('content-type', 'application/json')
+    //                                .send({ad_url: url});
+    //     response.should.have.status(200)
+    //     response.body.length.should.equal(1)
+    //     response.body[0].should.equal("Sports")
             
-    })
+    // }) ** Relies on external service (the web categorisation API used by the server to categorise ads)
 
     it("It should return No Category", async ()=>{
         const url = "not-a-valid-url"
